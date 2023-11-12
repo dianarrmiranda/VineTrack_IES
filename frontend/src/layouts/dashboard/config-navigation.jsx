ff3012a49b60a63c00f09258221fca21b47bbb13
dianarrmiranda <dianarrmiranda@ua.pt>
2023-11-12 21:03:28 +0000

    Remove blog related code

--- frontend/src/layouts/dashboard/config-navigation.jsx
+++ frontend/src/layouts/dashboard/config-navigation.jsx
@@ -26,11 +26,6 @@ const navConfig = [
     icon: icon("ic_vine"),
   },
   {
-    title: "blog",
-    path: "/blog",
-    icon: icon("ic_blog"),
-  },
-  {
     title: "login",
     path: "/login",
     icon: icon("ic_lock"),
--- frontend/src/routes/sections.jsx
+++ frontend/src/routes/sections.jsx
@@ -4,7 +4,6 @@ import { Outlet, Navigate, useRoutes } from "react-router-dom";
 import DashboardLayout from "src/layouts/dashboard";
 
 export const IndexPage = lazy(() => import("src/pages/app"));
-export const BlogPage = lazy(() => import("src/pages/blog"));
 export const UserPage = lazy(() => import("src/pages/user"));
 export const LoginPage = lazy(() => import("src/pages/login"));
 export const VinesPage = lazy(() => import("src/pages/vines"));
@@ -26,7 +25,6 @@ export default function Router() {
         { element: <IndexPage />, index: true },
         { path: "user", element: <UserPage /> },
         { path: "vines", element: <VinesPage /> },
-        { path: "blog", element: <BlogPage /> },
       ],
     },
     {
