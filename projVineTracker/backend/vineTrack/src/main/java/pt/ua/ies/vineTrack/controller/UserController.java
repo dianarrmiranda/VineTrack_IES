26dacd924d322ed0b57fdf4d7f96ba2af5a26251
dianarrmiranda <dianarrmiranda@ua.pt>
2023-11-17 23:09:32 +0000

    Add app and client services to docker-compose.yml

---
+++ projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/controller/UserController.java
@@ -0,0 +1,58 @@
+package pt.ua.ies.vineTrack.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pt.ua.ies.vineTrack.service.UserService;
+import pt.ua.ies.vineTrack.entity.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(path = "/register")
+    public User registerUser(@RequestBody User user){
+        return userService.save(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public User viewUser(@PathVariable Integer id){
+        return userService.getUserById(id);
+    }
+
+    @GetMapping(path = "/all")
+    public List<User> viewAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "/login/{email}/{password}")
+    public User loginUser(@PathVariable String email, @PathVariable String password){
+        User user = userService.getUserByEmail(email);
+        if(user.getPassword().equals(password)){
+            return user;
+        }
+        return null;
+    }
+
+
+    @PutMapping(path = "/update")
+    public User updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public String deleteUser(@PathVariable Integer id){
+        return userService.deleteUserById(id);
+    }
+}
---
+++ projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/service/UserService.java
@@ -0,0 +1,56 @@
+package pt.ua.ies.vineTrack.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pt.ua.ies.vineTrack.repository.UserRepo;
+import pt.ua.ies.vineTrack.entity.User;
+
+import java.util.List;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public User save(User user){
+        return userRepo.save(user);
+    }
+
+    public User getUserByEmail(String email){
+        return userRepo.findByEmail(email);
+    }
+
+    public User getUserById(Integer id){
+        return userRepo.findById(id).orElse(null);
+    }
+
+    public User getUserByName(String name){
+        return userRepo.findByName(name);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepo.findAll();
+    }
+
+    public String deleteUserById(Integer id){
+        userRepo.deleteById(id);
+        return "User removed! "+id;
+    }
+
+    public User getUserByRole(String role){
+        return userRepo.findByRole(role);
+    }
+
+    public User updateUser(User user){
+        User existingUser = userRepo.findById(user.getId()).orElse(null);
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setRole(user.getRole());
+        return userRepo.save(existingUser);
+    }
+    
+}
