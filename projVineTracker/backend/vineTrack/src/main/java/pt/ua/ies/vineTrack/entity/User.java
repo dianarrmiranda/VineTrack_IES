618cb4d3bb3f72ad44728576d4da2c8e690d8c94
dianarrmiranda <dianarrmiranda@ua.pt>
2023-11-24 19:35:43 +0000

    Fix that bc have things that belong another brach

--- projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/entity/User.java
+++ projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/entity/User.java
@@ -11,10 +11,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name = "user")
