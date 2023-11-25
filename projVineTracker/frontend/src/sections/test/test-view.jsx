c677b374fcd66dbb7de060102996697dbdae73af
Zé Mendes <mendes.j@ua.pt>
2023-11-25 03:12:11 +0000

    Modified Sockjs on react

--- projVineTracker/frontend/src/sections/test/test-view.jsx
+++ projVineTracker/frontend/src/sections/test/test-view.jsx
@@ -1,33 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { fetchData } from "src/utils";
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
 
 export default function TestView() {
+    // Fetch Data State
     const [data, setData] = useState([]);
 
     useEffect(() => {
         const fetchDataFromAPI = async () => {
             try {
-                const response = await fetchData('vine/test'); // Assuming fetchData is an async function
-
-                // Assuming the response is JSON data; modify this part based on your API response format
+                const response = await fetchData('vine/test');
                 setData(response);
+                console.log(response);
             } catch (error) {
                 console.error('Error during API call', error);
             }
         };
 
         fetchDataFromAPI();
-    }, []); // Empty dependency array to execute only once on component mount
+    }, []);
+
+    // WebSocket
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const socket = new SockJS('http://localhost:8080/ws');
+        const stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, () => {
+            stompClient.subscribe('/track/updates', (message) => {
+                setMessages((prevMessages) => [...prevMessages, message.body]);
+            });
+        });
+
+        return () => {
+            if (stompClient.connected) {
+                stompClient.disconnect();
+            }
+        };
+    }, [messages]); // Include messages in the dependency array
 
     return (
         <>
-            {data.map((item) => (
-                <div key={item.id}>
-                    <h1>{item.name}</h1>
-                    <p>{item.size}</p>
-                    <p>{item.user.name}</p>
-                </div>
-            ))}
+            <h2>Received:</h2>
+            <ul>
+                {messages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                ))}
+            </ul>
         </>
     );
 }
