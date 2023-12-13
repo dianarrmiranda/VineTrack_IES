aa9e3409b10dd57fb1136c1994a4ff206d8b8984
Diana <dianarrmiranda@gmail.com>
2023-12-13 22:16:03 +0000

    Remove unused import and variable in RabbitmqHandler.java and TrackRepo.java

--- projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/message_broker/RabbitmqHandler.java
+++ projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/message_broker/RabbitmqHandler.java
@@ -35,7 +35,6 @@ public class RabbitmqHandler {
     private TrackService trackService;
 
     private static final int MAX_NOTIFICATIONS = 10;
-    private static final double MAX_WATER_CONSUMPTION = 0.95; // max of 0.95L per m^2 per day
 
     @RabbitListener(queues = Config.QUEUE_NAME)
     public void receiveMessage(String message) {
