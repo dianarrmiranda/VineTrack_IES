914e51ad49edbfca367c9bd380b033402bc69794
Diana <dianarrmiranda@gmail.com>
2023-12-15 19:59:59 +0000

    Add Swagger annotations to TrackController

--- projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/controller/TrackController.java
+++ projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/controller/TrackController.java
@@ -9,25 +9,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+
 import pt.ua.ies.vineTrack.entity.Track;
 import pt.ua.ies.vineTrack.service.TrackService;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/api/tracks")
+@Tag(name = "Track", description = "Operations for tracks")
 public class TrackController {
 
     @Autowired
     TrackService trackService;
 
     @GetMapping()
+    @Operation(summary = "Get all tracks")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+        @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+    })
     public ResponseEntity<List<Track>> getAllVines(){
         try {
             return ResponseEntity.ok(trackService.getAllTracks());
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
-
     }
     
 }
