61be0ab418eb6c40d25519c0813e168a8614fe16
Diana <dianarrmiranda@gmail.com>
2023-12-15 19:55:50 +0000

    Add Swagger annotations to NutrientController

--- projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/controller/NutrientController.java
+++ projVineTracker/backend/vineTrack/src/main/java/pt/ua/ies/vineTrack/controller/NutrientController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+
 import pt.ua.ies.vineTrack.entity.Nutrient;
 import pt.ua.ies.vineTrack.entity.Track;
 import pt.ua.ies.vineTrack.service.NutrientService;
@@ -17,12 +23,18 @@ import pt.ua.ies.vineTrack.service.TrackService;
 @CrossOrigin("*")
 @RestController
 @RequestMapping(path = "api/nutrients")
+@Tag(name = "Nutrient", description = "Operations for nutrients")
 public class NutrientController {
 
     @Autowired
     NutrientService nutrientService;
 
     @GetMapping()
+    @Operation(summary = "Get all nutrients")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+        @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+    })
     public ResponseEntity<List<Nutrient>> getAllNutrients(){
         try {
             return ResponseEntity.ok(nutrientService.getAllNutrients());
